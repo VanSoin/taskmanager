6dc1166920604e9f3791235ff33b5eece28f6270
VanSoin <vanshikasoin2@gmail.com>
2026-04-19 13:21:13 +0530

    initial: spring boot task manager setup with H2 database

---
+++ src/test/java/com/vanshika/taskmanager/TaskmanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vanshika.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskmanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
