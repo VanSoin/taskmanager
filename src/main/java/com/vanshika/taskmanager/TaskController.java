f49d407bb5c71ae7c2456a8dfe0480801354bf56
VanSoin <vanshikasoin2@gmail.com>
2026-05-02 21:41:09 +0530

    feat: Added TaskController with POST and GET endpoints. Tested with postman

---
+++ src/main/java/com/vanshika/taskmanager/TaskController.java
@@ -0,0 +1,25 @@
+package com.vanshika.taskmanager;
+
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/tasks")
+public class TaskController {
+
+    private final TaskService taskService;
+
+    public TaskController(TaskService taskService) {
+        this.taskService = taskService;
+    }
+
+    @GetMapping
+    public List<Task> getAllTasks() {
+        return taskService.getAllTasks();
+    }
+
+    @PostMapping
+    public Task createTask(@RequestBody Task task) {
+        return taskService.createTask(task);
+    }
+}
